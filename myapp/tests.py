812921e1909084d1a046c801658d690c14b8c955
Arsen791 <114273777+Arsen791@users.noreply.github.com>
2023-11-29 09:09:53 +0600

    eddit test.py

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from myapp.models import zat
 
-# Create your tests here.
+class zatTestCase(TestCase):
+    def setUp(self):
+        zat.objects.create(name="Dala", price=100)
+
+    def test_model_creation(self):
+        obj = zat.objects.get(name="Dala")
+        self.assertEqual(obj.price, 100)
+
+    def test_model_update(self):
+        obj = zat.objects.get(name="Dala")
+        obj.price = 200
+        obj.save()
+        updated_obj = zat.objects.get(name="Dala")
+        self.assertEqual(updated_obj.price, 200)
+
+    def test_model_deletion(self):
+        obj = zat.objects.get(name="Dala")
+        obj.delete()
+        with self.assertRaises(zat.DoesNotExist):
+            zat.objects.get(name="Dala")
+
+    def test_model_query(self):
+        zat.objects.create(name="Another", price=150)
+        queryset = zat.objects.filter(price__gte=100)  # Получение объектов с ценой >= 100
+        self.assertEqual(queryset.count(), 2)  # Ожидается, что найдено 2 объекта
